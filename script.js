02576a1220cac2dfe8200a45e141b0556e7811e1
Rafika_MK <rafikahouda2005@gmail.com>
2024-08-25 22:27:58 +0100

    I managed to complete the css finally

---
+++ script.js
@@ -0,0 +1,6 @@
+function add (firstNum,secondNum){return(firstNum+secondNum);}
+function sub (firstNum,secondNum){return(firstNum-secondNum);}
+function mul (firstNum,secondNum){return(firstNum*secondNum);}
+function div (firstNum,secondNum){return(firstNum/secondNum);}
+
+let firstNum ,secondNum ,operator;
